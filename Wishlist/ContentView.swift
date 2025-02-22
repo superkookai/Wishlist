777159aec4d90c04a0b034345054fe55564e9209
Weerawut <weerawut@live.com>
2025-02-22 12:39:34 +0700

    Initial Commit

---
+++ Wishlist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wishlist
+//
+//  Created by Weerawut Chaiyasomboon on 22/2/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Weerawut Chaiyasomboon on 22/2/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
